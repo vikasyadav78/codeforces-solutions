f9f2da9ff14aa56dcedd718b23e5a82c9f0ad326
peace <princy.krishnakeshav@gmail.com>
2016-04-24 15:12:51 +0530

    solution to GSS1 and GSS3 in spoj using segment tree

--- spoj/GSS1.cpp
+++ spoj/GSS1.cpp
@@ -2,15 +2,15 @@
 #include<cstdio>
 using namespace std;
 #define ll long long
-#define MAX 1001
+#define MAX 100001
 struct node {
     ll left;
     ll right;
     ll sum;
     ll best;
 };
-ll a[MAX];
-node tree[3*MAX];
+ll a[50009];
+node tree[200009];
 
 node makeNode(ll left , ll right , ll sum , ll best)
 {
@@ -27,9 +27,9 @@ node combine(node L , node R)
     if(L.sum + R.left > left) left = L.sum + R.left;
     ll right = R.right;
     if(R.sum + L.right > right) right = R.sum + L.right;
-    ll sum = L.sum + R.sum;
+    //ll sum = L.sum + R.sum;
     ll best = max(L.right + R.left , max(L.best , R.best));
-    return makeNode(left , right , sum , best);
+    return makeNode(left , right , L.sum + R.sum , best);
 }
 node build(ll from , ll to , ll idx)
 {
@@ -53,11 +53,24 @@ node query(ll index , ll from , ll to , ll a , ll b)
     ll mid = (from + to)/2;
     if(b <= mid)
         return query(2*index , from , mid , a , b);
-    if(a >= mid)
+    if(a > mid)
         return query(2*index+1 , mid + 1 , to , a , b);
-    node l = query(index*2 , from , mid , a , b);
-    node r = query(index*2 + 1 , mid + 1 , to , a , b);
+    node l = query(index*2 , from , mid , a , mid);
+    node r = query(index*2 + 1 , mid + 1 , to , mid + 1 , b);
     return combine(l , r);
+}
+void update(int from, int to, int index, int indexMod) {
+    if (from == to && from == indexMod) {
+        tree[index] = makeNode(a[from], a[from], a[from], a[from]);
+        return;
+    }
+    int mid = (from+to)/2;
+    if (indexMod <= mid) {
+        update(from, mid, (index<<1), indexMod);
+    } else {
+        update(mid+1, to, (index<<1)+1, indexMod);
+    }
+    tree[index] = combine(tree[index<<1],tree[(index<<1)+1]);
 }
 ll n;
 int main()
@@ -66,12 +79,18 @@ int main()
     for(ll i = 1 ; i <= n ; i++)
         scanf("%lld",&a[i]);
     build(1 , n , 1);
-    ll t , L , R;
+    ll t , L , R , type;
     scanf("%lld",&t);
     for(ll i = 0 ; i < t ; ++i)
     {
-        scanf("%lld %lld",&L , &R);
-        printf("%lld\n" , query(1 , 1 , n , L , R).best);
+        scanf("%lld %lld %lld",&type , &L , &R);
+        if(type == 0)
+        {
+            a[L] = R;
+            update(1 , n , 1 , L);
+        }
+        else
+            printf("%lld\n" , query(1 , 1 , n , L , R).best);
     }
     return 0;
 }
