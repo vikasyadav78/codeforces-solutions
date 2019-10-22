d00ec08226d61074a10fa6e9d18d312bb7b45b06
vikasyadav78 <56009683+vikasyadav78@users.noreply.github.com>
2019-10-23 00:58:42 +0530

    Update 115A.cpp

--- 115A.cpp
+++ 115A.cpp
@@ -6,7 +6,7 @@ int main()
     //cout << "Hello World!" << endl;
     int n , ans = 0 , count = 0;
     cin >> n;
-    int a[n+9];
+    int a[n+9];  //Creating an array of size a
     for(int i = 1 ; i <= n ;i++)
         cin >> a[i];
     for(int i = 1 ; i <= n ;i++)
@@ -15,12 +15,12 @@ int main()
         int x = a[i];
         while(x != -1)
         {
-            x = a[x];
+            x = a[x];      
             count++;
         }
-        ans = max(ans , count);
+        ans = max(ans , count);    //Finding maximum between ans and count
     }
-    cout << ans+1;
+    cout << ans+1;  // Incrementing ans by 1
     return 0;
 }
 
